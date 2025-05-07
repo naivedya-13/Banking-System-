12e73b8d21948acb6d7f82463dbfd2b68e44f403
Naivedya Tripathi <165983020+naivedya-13@users.noreply.github.com>
2025-05-07 20:24:08 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE = "http://localhost:5000/api";
-const API_BASE = "https://banking-system-server-rho.vercel.app/";
+const API_BASE = "https://banking-system-server-rho.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_BASE,
